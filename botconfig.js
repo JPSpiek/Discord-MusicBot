4dc9db28163c4ad7e3fb7b304b4808c2b465f1a9
JPSpiek <126972232+JPSpiek@users.noreply.github.com>
2023-03-04 21:19:58 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -3,8 +3,8 @@ module.exports = {
   ExpressServer: true, // If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
+  SupportServer: "https://discord.gg/", // Support Server Link
+  Token: process.env.Token || "MTA4MTQwNzE2NTY4NTg0NjAxNg.GfTpiJ.KvXVKQLA0ONQs6gEwIWZVahySFOPHGTup8oVn0", // Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", // Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
